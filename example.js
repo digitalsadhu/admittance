f426552e90e2a025f46e143e32341228ec764b2d
Richard Walker <digitalsadhu@gmail.com>
2014-03-06 21:53:44 +0100

    Add basic example file to work toward

---
+++ example.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var fs              = require('fs')
+  , admittance      = require('admittance')
+ 
+//read from the text file where we store pairs of user ids and permissions
+var storeR = fs.createReadStream(__dirname + '/my-store.txt')
+ 
+//read in permissions
+storeR.pipe(admittance)
+ 
+//do a permissions check
+if (admittance(userid).is('admin')) {
+    //yay, user is admin!
+}
